4ea076e28fd6b2069344dabf2a84ea7cfea43df1
hyun2y00 <pottq577@naver.com>
2025-04-18 15:25:25 +0900

    Style(Auth): [Lv5] signin 메소드 내부 줄바꿈 수정

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -47,7 +47,8 @@ public class AuthService {
     @Transactional(readOnly = true)
     public SigninResponse signin(SigninRequest signinRequest) {
         User user = userRepository.findByEmail(signinRequest.getEmail()).orElseThrow(
-                () -> new InvalidRequestException("가입되지 않은 유저입니다."));
+                () -> new InvalidRequestException("가입되지 않은 유저입니다.")
+        );
 
         // 로그인 시 이메일과 비밀번호가 일치하지 않을 경우 401을 반환합니다.
         if (!passwordEncoder.matches(signinRequest.getPassword(), user.getPassword())) {
