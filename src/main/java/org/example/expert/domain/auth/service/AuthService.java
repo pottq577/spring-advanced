f1d4b9f1bccdb00c1489263336d60f308be5a2d8
hyun2y00 <pottq577@naver.com>
2025-04-20 22:04:32 +0900

    Feat(Auth): [Lv5] 예외 메세지 enum 사용

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -8,6 +8,7 @@ import org.example.expert.domain.auth.dto.request.SignupRequest;
 import org.example.expert.domain.auth.dto.response.SigninResponse;
 import org.example.expert.domain.auth.dto.response.SignupResponse;
 import org.example.expert.domain.auth.exception.AuthException;
+import org.example.expert.domain.common.exception.ExceptionCode;
 import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
@@ -27,7 +28,7 @@ public class AuthService {
     public SignupResponse signup(SignupRequest signupRequest) {
 
         if (userRepository.existsByEmail(signupRequest.getEmail())) {
-            throw new InvalidRequestException("이미 존재하는 이메일입니다.");
+            throw new InvalidRequestException(ExceptionCode.ALREADY_USED_EMAIL);
         }
 
         String encodedPassword = passwordEncoder.encode(signupRequest.getPassword());
@@ -48,11 +49,11 @@ public class AuthService {
     @Transactional(readOnly = true)
     public SigninResponse signin(SigninRequest signinRequest) {
         User user = userRepository.findByEmail(signinRequest.getEmail()).orElseThrow(
-                () -> new InvalidRequestException("가입되지 않은 유저입니다."));
+                () -> new InvalidRequestException(ExceptionCode.NOT_SIGNUP_USER));
 
         // 로그인 시 이메일과 비밀번호가 일치하지 않을 경우 401을 반환합니다.
         if (!passwordEncoder.matches(signinRequest.getPassword(), user.getPassword())) {
-            throw new AuthException("잘못된 비밀번호입니다.");
+            throw new AuthException(ExceptionCode.PASSWORD_INCORRECT);
         }
 
         String bearerToken = jwtUtil.createToken(user.getId(), user.getEmail(), user.getUserRole());
